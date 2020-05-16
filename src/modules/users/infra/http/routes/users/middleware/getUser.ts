344c001742f7cc50626ffce964318b5de8c01fae
Guilherme Costa Arantes <arantes.g@outlook.com>
2020-05-16 12:26:48 -0300

    added fields to getuser

--- src/modules/users/infra/http/routes/users/middleware/getUser.ts
+++ src/modules/users/infra/http/routes/users/middleware/getUser.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from 'express';
 
 import UsersRepository from '../../../../database/repositories/UsersRepository';
+import IUser from '../../../../../entities/IUser';
 
 export default async (req: Request, res: Response): Promise<void> => {
   let { id } = req.params;
+  let fields = ['id', 'fullName', 'avatar'] as (keyof IUser)[];
   if (id === 'me') {
     id = req.tokenUserId;
+    fields = [];
   }
   const usersRepository = new UsersRepository();
-  const user = await usersRepository.findById(id, ['id']);
+  const user = await usersRepository.findById(id, fields);
   res.status(200).send(user);
 };
