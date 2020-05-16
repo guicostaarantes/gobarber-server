08e43c6152cde2200d1122be6207c08ced72b6f7
Guilherme Costa Arantes <arantes.g@outlook.com>
2020-05-16 10:37:52 -0300

    added fields to getsupplier

--- src/modules/suppliers/infra/http/routes/suppliers/middleware/getSuppliers.ts
+++ src/modules/suppliers/infra/http/routes/suppliers/middleware/getSuppliers.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from 'express';
 
 import SuppliersRepository from '../../../../database/repositories/SuppliersRepository';
+import ISupplier from '../../../../../entities/ISupplier';
 
 export default async (req: Request, res: Response): Promise<void> => {
-  const { page = 1, latitude, longitude } = req.query;
+  const { page = 1, position, fields } = req.query;
+  const [latitude, longitude] = (position as string).split(',');
+  const fieldsArray = (fields as string).split(',') as (keyof ISupplier)[];
   const suppliersRepository = new SuppliersRepository();
   const suppliers = await suppliersRepository.findByNearestLocation(
     +page,
     +latitude,
     +longitude,
-    [],
+    fieldsArray,
   );
   res.status(200).send(suppliers);
 };
